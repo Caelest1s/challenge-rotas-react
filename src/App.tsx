2ad295b707f30200a88b82bdb6617fe5786de0a4
Jefferson Celestino <jefferson.ucb02@gmail.com>
2024-04-06 21:03:24 -0300

    Not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./routers/Home"
 import AboutUs from "./routers/Home/AboutUs"
+import NotFound from "./routers/NotFound"
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} >
             <Route index element={<AboutUs />} />
+            
+          <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/routers/NotFound/index.tsx
@@ -0,0 +1,7 @@
+import Message from "../../components/Message";
+
+export default function NotFound(){
+    return(
+        <Message text="Não encontrado!" />
+    );
+}
